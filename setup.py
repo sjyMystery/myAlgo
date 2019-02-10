0eeccffe43b5410f01bfe7ac9e83d6d5ca8184f9
nathaniel <348831271@qq.com>
2019-02-10 11:34:11 +0800

    first version.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myALGO',
+    version='1.0',
+    packages=['bar', 'feed', 'event', 'order', 'utils', 'broker', 'config', 'logger', 'strategy', 'stratanalyzer'],
+    url='',
+    license='',
+    author='nathaniel',
+    author_email='348831271@qq.com',
+    description=''
+)
