57e569655b21ca695e2422fab905610166c7055d
nathaniel <348831271@qq.com>
2019-02-12 02:55:19 +0800

    fix a bug.

--- myalgo/__init__.py
+++ myalgo/__init__.py
@@ -1,7 +1,7 @@
-import broker
-import bar
-import event
-import feed
-import order
-import strategy
-import stratanalyzer
+import myalgo.broker as broker
+import myalgo.bar as bar
+import myalgo.event as event
+import myalgo.feed as feed
+import myalgo.order as order
+import myalgo.strategy as strategy
+import myalgo.stratanalyzer as stratanalyzer
